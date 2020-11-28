47608cf1afc76a4c55e81fad354f3cbe45528f31
Taran <21302579+tarantej@users.noreply.github.com>
2020-11-28 13:56:41 +1300

    Re-adding missing files

---
+++ build/precache-manifest.16ab0b39756493b5dbe2aecac8a5f6ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621ab5ff238b8615926dd4c431505e4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc0a91970ed0c3d1adc1",
+    "url": "/static/css/main.de11a90b.chunk.css"
+  },
+  {
+    "revision": "6f57a75d18d9f4926255",
+    "url": "/static/js/2.f709ebbc.chunk.js"
+  },
+  {
+    "revision": "c8e07e19562a4690a354be24150f8167",
+    "url": "/static/js/2.f709ebbc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc0a91970ed0c3d1adc1",
+    "url": "/static/js/main.b6625eb8.chunk.js"
+  },
+  {
+    "revision": "ae744f3fef1e05f95e89",
+    "url": "/static/js/runtime-main.65e5f6ea.js"
+  },
+  {
+    "revision": "c130f2f6860bfcea7924002a0aea53ed",
+    "url": "/static/media/logo.c130f2f6.png"
+  }
+]);
